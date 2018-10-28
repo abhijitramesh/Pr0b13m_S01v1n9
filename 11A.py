e28d4b0961b876bc825518e05b37f332f79b8486
abhijitramesh <43090559+abhijitramesh@users.noreply.github.com>
2018-10-28 22:51:22 +0530

    Update
    commented

--- 11A.py
+++ 11A.py
@@ -1,8 +1,8 @@
 import math
-n,m=input().split()
-n,m=int(n),int(m)
+n,m=input().split()#splliting the input into two
+n,m=int(n),int(m)#assigining the spllited input into two varables
 count=0
-a=list(map(int,input().split()))
+a=list(map(int,input().split()))#assiging input as a list
 for i in range(n-1):
     if a[i]>=a[i+1]:   
            count+=math.ceil(((a[i]-a[i+1])+1)/m)
