d969a037e973cc44cbafb5761b13a97411b56e92
abhijitramesh <43090559+abhijitramesh@users.noreply.github.com>
2018-10-28 22:49:20 +0530

    comment
    commented

--- 110A.py
+++ 110A.py
@@ -1,6 +1,6 @@
-p=int(input())
-k=str(p)
-if str(k.count('7')+k.count('4')).count('7')+str(k.count('7')+k.count('4')).count('4')==len(str(k.count('7')+k.count('4'))):
+p=int(input())#inputting a value
+k=str(p)#assigning the value as string
+if str(k.count('7')+k.count('4')).count('7')+str(k.count('7')+k.count('4')).count('4')==len(str(k.count('7')+k.count('4'))):#cheaking condition
     print("YES")
 else:
     print("NO")
